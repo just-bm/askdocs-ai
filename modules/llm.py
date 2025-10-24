d0f96fa1c0dd66672d598a57b20a174f14c0e643
Balaji-R-05 <balajiramu23@gmail.com>
2025-10-25 00:13:31 +0530

    Updated llm model

--- modules/llm.py
+++ modules/llm.py
@@ -1,9 +1,14 @@
 import os
 from langchain_groq import ChatGroq
 from langchain.chains import RetrievalQA
-from dotenv import load_dotenv
+from dotenv import load_dotenv, find_dotenv
 
-load_dotenv()
+dotenv_path = find_dotenv()
+load_dotenv(dotenv_path, override=True)
+
+GROQ_API_KEY = os.getenv("GROQ_API_KEY")
+
+print(f"GROQ_API_KEY loaded: {GROQ_API_KEY}")
 
 if not os.environ.get("GROQ_API_KEY"):
     raise ValueError("⚠️ GROQ_API_KEY not found. Please set it in .env")
@@ -11,7 +16,7 @@ if not os.environ.get("GROQ_API_KEY"):
 def get_llm_chain(vectorstore):
     llm = ChatGroq(
         groq_api_key=os.environ.get("GROQ_API_KEY"),
-        model_name="llama3-8b-8192",
+        model_name="openai/gpt-oss-120b",
         temperature=0.3,
     )
     retriever = vectorstore.as_retriever(search_kwargs={"k": 3})
